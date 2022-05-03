c0250f85ffb714662b72980e7ae034ec85041d83
Rowings <zakariaazarkan@gmail.com>
2022-05-03 16:21:16 +0200

    ajout sol qui bouge, pas encore au top peut etre redimenssioner la page

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // const pour selectionné le dino
 const dino = document.getElementById("dino");
+// const pour selectionné le cactus
+const cactus = document.getElementById("cactus")
+
 
 // fonction qui s'occupe du saut
 function jump() {
@@ -14,9 +17,18 @@ function jump() {
 }
 //fonction pour avoir le statut du dino 
 let isAlive = setInterval(function () {
-    //permet de savoir ou se trouve le dino par rapport au top
+    //permet de savoir ou se trouve le dino par rapport au top (y position)
     // parseInt sert à arrondir les resultat lors du saut
     let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+        // permet de savoir ou se trouve le cactus sur l'axe X 
+        let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+        
+    // dection des collision
+
+    if(cactusLeft <50 && cactusLeft > 0 && dinoTop >= 140) {
+        
+        // alert("Perdu poto !");
+    }
     
 },10);
 
