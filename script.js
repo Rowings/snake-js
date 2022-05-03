ae59f776be542533bb05a703f35b445fc2cfd7a5
Rowings <zakariaazarkan@gmail.com>
2022-05-03 15:15:33 +0200

    ajout du cactus qui bouge de droite à gauche

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+// const pour selectionné le dino
 const dino = document.getElementById("dino");
 
+// fonction qui s'occupe du saut
 function jump() {
+    // empecher le saut de stopper en cas de spam
         if(dino.classList != "jump") { 
     dino.classList.add("jump");
 
